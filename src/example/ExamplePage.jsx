6f2ea9c18eaacf3ed315d964187a17ae91da8fd5
thetansabe <thetannguyen193@gmail.com>
2024-01-14 22:40:14 +0700

    add responsive

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,6 +1,8 @@
 import { Container } from "@edx/paragon";
 import SideBar from "./components/sidebar/SideBar";
 import Chat from "./components/chat/Chat";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHamburger } from "@fortawesome/free-solid-svg-icons";
 
 const handleInputMessage = (event) => {
   console.log(event.target.innerHTML);
@@ -12,6 +14,10 @@ const ExamplePage = () => (
       <div className="container">
         {/* Left */}
         <SideBar />
+
+        <div className="hamburger">
+          <FontAwesomeIcon icon={faBars}/>
+        </div>
 
         {/* Right */}
         <Chat />
